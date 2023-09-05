5e8237149d4f965b6a92a855a2aa861fe5ccd981
ashimoo <59338930+ashimoo@users.noreply.github.com>
2023-09-05 14:37:23 +0100

    Update run_model_universal.py

--- run_model_universal.py
+++ run_model_universal.py
@@ -78,11 +78,11 @@ def main(dataset,class_loss_weight,model,mkt_state,seed,gpu):
         loss, hawkes_loss, pred_accuracy = model.compute_loss(batch_dict, intens, cells, cell_targets, outputs, decays, args.class_loss_weight)
         loss.backward()
         optimizer.step()
-        average_loss.append(hawkes_loss)
-        accuracy_list.append(pred_accuracy)
+        average_loss.append(hawkes_loss*len(batch_dict['time_step']))
+        accuracy_list.append(pred_accuracy*len(batch_dict['time_step']))
         if itr % data_obj['n_train_batches'] == 0:
             torch.cuda.empty_cache()
-            logger.write('Epoch {:04d} Hawkes_loss: {:.4f} Accuracy_score: {:.4f}'.format(itr // data_obj['n_train_batches'],sum(average_loss) / len(average_loss),sum(accuracy_list) / len(accuracy_list)))
+            logger.write('Epoch {:04d} Hawkes_loss: {:.4f} Accuracy_score: {:.4f}'.format(itr // data_obj['n_train_batches'],sum(average_loss) / len(train_record_list),sum(accuracy_list) / len(train_record_list)))
             if itr % (10 * data_obj['n_train_batches']) == 0:
                 target_array = []
                 pred_array_known_time = []
@@ -93,12 +93,12 @@ def main(dataset,class_loss_weight,model,mkt_state,seed,gpu):
                     target, pred_known_time, hawkes_loss_val= model.read_predict(batch_dict, args.mkt_state, metrics=False)
                     target_array.append(target)
                     pred_array_known_time.append(pred_known_time)
-                    hawkes_loss_val_list.append(hawkes_loss_val)
+                    hawkes_loss_val_list.append(hawkes_loss_val*len(batch_dict['time_step']))
                 target_array = np.hstack(target_array).reshape(-1)
                 pred_array_known_time = np.hstack(pred_array_known_time).reshape(-1)
-                message = 'Validation Hawkes_loss {:.4f} Accuracy_score_known_t {:.4f}'.format((sum(hawkes_loss_val_list) / len(hawkes_loss_val_list)), accuracy_score(target_array,pred_array_known_time))
+                message = 'Validation Hawkes_loss {:.4f} Accuracy_score_known_t {:.4f}'.format((sum(hawkes_loss_val_list) / len(val_record_list)), accuracy_score(target_array,pred_array_known_time))
                 logger.write(message)
-                val_accuracy_list_known_t.append(sum(hawkes_loss_val_list) / len(hawkes_loss_val_list))
+                val_accuracy_list_known_t.append(sum(hawkes_loss_val_list) / len(val_record_list))
                 if val_accuracy_list_known_t[-1] == np.min(val_accuracy_list_known_t):
                     torch.save(model,model_path_known_t)
             accuracy_list = []
@@ -116,12 +116,12 @@ def main(dataset,class_loss_weight,model,mkt_state,seed,gpu):
         target_array.append(target)
         pred_array_known_time.append(pred_known_time)
         pred_array_unknown_time.append(pred_unknown_time)
-        hawkes_loss_test_list.append(hawkes_loss_test)
-        error_dt_test_list.append(error_dt_test)
+        hawkes_loss_test_list.append(hawkes_loss_test*len(batch_dict['time_step']))
+        error_dt_test_list.append(error_dt_test*len(batch_dict['time_step']))
     target_array = np.hstack(target_array).reshape(-1)
     pred_array_known_time = np.hstack(pred_array_known_time).reshape(-1)
     pred_array_unknown_time = np.hstack(pred_array_unknown_time).reshape(-1)
-    message = 'Test Hawkes_loss {:.4f} Accuracy_score_known_t {:.4f} Accuracy_score_unknown_t {:.4f} Time error {:.4f}'.format((sum(hawkes_loss_test_list) / len(hawkes_loss_test_list)), accuracy_score(target_array,pred_array_known_time), accuracy_score(target_array,pred_array_unknown_time), (sum(error_dt_test_list) / len(error_dt_test_list)))
+    message = 'Test Hawkes_loss {:.4f} Accuracy_score_known_t {:.4f} Accuracy_score_unknown_t {:.4f} Time error {:.4f}'.format((sum(hawkes_loss_test_list) / len(test_record_list)), accuracy_score(target_array,pred_array_known_time), accuracy_score(target_array,pred_array_unknown_time), (sum(error_dt_test_list) / len(test_record_list)))
     logger.write(message)
     print(message)
     logger.close()
